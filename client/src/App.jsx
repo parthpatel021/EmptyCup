e7b621f7b0c022df8d5989fa41763b575755f3ef
Parth Patel <parthkumar111patel@gmail.com>
2023-11-08 00:00:11 +0530

    Navbar Component Added

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./app.css";
+import Navbar from './Components/Navbar';
+
+const App = () => {
+  return (
+    <div className='app'>
+        <Navbar />
+        App
+    </div>
+  )
+}
+
+export default App
